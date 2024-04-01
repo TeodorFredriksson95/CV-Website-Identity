cfb82b0ac44d31efc8163cbcc34a201969a1d271
Teodor Fredriksson <teo95@hotmail.se>
2024-04-02 01:15:59 +0200

    db update

--- CV.Identity/Database/DbInitializer.cs
+++ CV.Identity/Database/DbInitializer.cs
@@ -19,8 +19,17 @@ namespace CV.Identity.Database
 
         public async Task InitializeAsync()
         {
-            Console.WriteLine("database connection");
-            using var connection = await _dbConnectionFactory.CreateConnectionAsync();
+            try
+            {
+                Console.WriteLine("Attempting database connection...");
+                using var connection = await _dbConnectionFactory.CreateConnectionAsync();
+                Console.WriteLine("Database connection successful.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to connect to the database: {ex.Message}");
+                Console.WriteLine($"Exception details: {ex}");
+            }
         }
     }
 }
